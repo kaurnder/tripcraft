fea6879f6d423616f21e5506cb3b5935138e5f9c
pactice-git <practice@gmail.com>
2025-07-14 16:52:18 +0530

    ok-inder

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@ define('WP_DEBUG_DISPLAY', false);
  define('FS_METHOD', 'direct');
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'tripcraft' );
+define( 'DB_NAME', 'tripcraft-inder' );
 
 /** Database username */
 define( 'DB_USER', 'Dattar' );
